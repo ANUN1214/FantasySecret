35136eaafea7421d55f1d6b4252a92710aa69990
uyume <dakerj@qq.com>
2023-02-06 12:45:34 +0800

    commit, 2023/02/06 周一

--- 上传.bat
+++ 上传.bat
@@ -2,8 +2,6 @@ set user=uyume
 set str1=commit
 set str2=%date%
 set result=%str1%, %str2%
-git remote set-url --fetch origin https://gitclone.com/github.com/yuan-uyume/FantasySecret.git
-git remote set-url --push origin https://github.com/yuan-uyume/FantasySecret.git
 git pull
 git checkout -b %user% origin/%user%
 git add ./
