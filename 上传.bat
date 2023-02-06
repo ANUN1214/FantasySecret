4f89e686e3d5b80733be8e1518f7a04b60c0d757
uyume <dakerj@qq.com>
2023-02-06 16:23:27 +0800

    commit, 2023/02/06 周一

--- 上传.bat
+++ 上传.bat
@@ -2,21 +2,19 @@ set user=uyume
 set str1=commit
 set str2=%date%
 set result=%str1%, %str2%
-<<<<<<< HEAD
-<<<<<<< HEAD
 git add ./
 git commit -m "%result%"
 git pull
 git checkout -b %user% origin/%user%
 if errorlevel 1 goto Fail
 if errorlevel 0 goto Success
-pause ...
+pause
 
 :Success
 git branch -v
 git merge main
 git push origin %user% -f
-pause ...
+pause
 
 :Fail
 git checkout %user%
@@ -25,30 +23,4 @@ git add ./
 git commit -m "%result%"
 git merge main
 git push origin %user% -f
-=======
-git pull
-git checkout -b %user% origin/%user%
-git add ./
-git commit -m "%result%"
-git push -u origin %user%
->>>>>>> 35136eaafea7421d55f1d6b4252a92710aa69990
-=======
-git add ./
-git commit -m "%result%"
-git pull
-git checkout -b %user% origin/%user%
-if errorlevel 1 goto Fail
-git branch -v
-git merge main
-git push origin %user% -f
-pause ...
-
-:Fail
-git checkout %user%
-git branch -v
-git add ./
-git commit -m "%result%"
-git merge main
-git push origin %user% -f
->>>>>>> main
-pause ...
+pause
