6d60e082e7e827f155cd726cad525b99c54b6b83
uyume <dakerj@qq.com>
2023-02-06 16:17:43 +0800

    commit, 2023/02/06 周一

--- 上传.bat
+++ 上传.bat
@@ -4,8 +4,9 @@ set str2=%date%
 set result=%str1%, %str2%
 git pull origin main
 git checkout -b %user% origin/%user%
-git merge main
+git branch -v
 git add ./
 git commit -m "%result%"
+git merge main
 git push -u origin %user% -f
 pause ...
