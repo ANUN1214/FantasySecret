ea59487b65896a23c01fe8e033b236651af82aa7
uyume <dakerj@qq.com>
2023-02-06 17:01:54 +0800

    commit, 2023/02/06 周一

--- 更新.bat
+++ 更新.bat
@@ -1,2 +1,13 @@
+set user=uyume
 git pull
-pause ...
+git checkout -b %user% origin/%user%
+if errorlevel 1 goto Fail
+if errorlevel 0 goto Success
+
+:Success
+echo 更新成功!
+pause
+
+:Fail
+git checkout %user%
+pause
